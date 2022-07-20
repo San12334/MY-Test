3c1ea3155b6f911deb3cdd38d7178f7967342189
santhosh <santhoshkumar1233@gmail.com>
2022-07-20 08:29:38 +0530

    sample

---
+++ Test22/src/main/java/Mytest22/App.java
@@ -0,0 +1,13 @@
+package Mytest22;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
